3a2da22e931e6959747dc217bf5081c278997994
Enis Behiç Tuysuz <enisbtuysuz@gmail.com>
2017-09-14 12:34:54 +0300

    Removed one test and added Travis CI

--- tests/main.py
+++ tests/main.py
@@ -1,29 +1,12 @@
 import json
-import requests
-
 import validate_json
-
-def check_is_alive(url):
-    """ check url then if url isn't alive, add to file """
-    print(url)
-    try:
-        requests.get(url, allow_redirects=False, timeout=10.0)
-    except requests.exceptions.ConnectionError as exc:
-        print('- Website doesn\'t exists: ', exc)
-        with open('result_test.txt', 'a') as result_test:  # Appending urls
-            result_test.write(url + '\n')
+import unittest
 
 
-def main():
-    with open('result_test.txt', 'w') as result_test:  # Creating result_test.txt file
-        result_test.write('### Result Wrong Links\n')
-    validate_json.check_json_is_valid()
-    with open('../world_universities_and_domains.json') as json_raw:
-        universities = json.load(json_raw)
-        for university in universities[:]:
-            # TODO: Test all web pages
-            check_is_alive(university['web_pages'][0])
+class DomainsTests(unittest.TestCase):
+    def test_json_is_valid(self):
+        validate_json.check_json_is_valid()
 
 
 if __name__ == '__main__':
-    main()
+    unittest.main(verbosity=2)
