3c3ae87043126bbf4890ef2a3f6ef6bec435caf5
Enis Behiç Tuysuz <enisbtuysuz@gmail.com>
2017-09-14 14:26:21 +0300

    Fixed the url test and updated .travis.yml

--- tests/main.py
+++ tests/main.py
@@ -8,13 +8,16 @@ class DomainsTests(unittest.TestCase):
     def test_json_is_valid(self):
         validate_json.check_json_is_valid()
 
-    def check_is_alive(url):
+    def check_is_alive():
         """ check url then if url isn't alive, add to file """
-        print(url)
-        try:
-            requests.get(url, allow_redirects=False, timeout=10.0)
-        except requests.exceptions.ConnectionError as exc:
-            print('- Website doesn\'t exists: ', exc)
+        with open('../world_universities_and_domains.json') as json_raw:
+            universities = json.load(json_raw)
+        for university in universities[:]:
+            try:
+                # TODO: Test with multiple domains
+                requests.get(university['web_pages'][0], allow_redirects=False, timeout=10.0)
+            except requests.exceptions.ConnectionError as exc:
+                print('- Website doesn\'t exists: ', exc)
 
 
 if __name__ == '__main__':
