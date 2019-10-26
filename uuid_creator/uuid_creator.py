73361d9e3c431662a30d225c9c3f02c6ddeefa53
tayyipgoren <tayyipgoren@gmail.com>
2019-10-26 14:29:16 +0300

    Added uuid creator

---
+++ uuid_creator/uuid_creator.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+import sys
+import argparse
+import uuid
+import json
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input_file", type=str,
+                        help="Input file (JSON)")
+    parser.add_argument("output_file", type=str,
+                        help="Output file (JSON)")
+
+    args = parser.parse_args()
+
+    with open(args.input_file, 'r', encoding='utf-8') as in_file:
+        json_data = json.load(in_file)
+        
+        for school in json_data:
+            school['uuid'] = uuid.uuid4().__str__()
+        
+        with open(args.output_file, 'wb', encoding='utf-8') as out_file:
+            json.dump(json_data, out_file)
