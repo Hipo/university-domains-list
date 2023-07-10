4c288fcf2fbad65df81d522c3ebf52d80cc4503f
aviiciii <laavesh1@gmail.com>
2023-07-10 20:28:53 +0530

    add validation error message

--- tests/test_domains.py
+++ tests/test_domains.py
@@ -9,18 +9,20 @@ class DomainsTests(unittest.TestCase):
         with open("world_universities_and_domains.json", encoding="utf-8") as json_file:
             valid_json = json.load(json_file)
         for university in valid_json:
-            self.assertIn("name", university)
-            self.assertIn("domains", university)
-            self.assertIsInstance(university["domains"], list)
+            self.assertIn("name", university, msg="University Name is missing")
+            self.assertIn("domains", university, msg="University Domains are missing")
+            self.assertIsInstance(university["domains"], list, msg="University Domains must be a list")
             for domain in university["domains"]:
-                self.assertTrue(validators.domain(domain))
-            self.assertIn("web_pages", university)
-            self.assertIsInstance(university["web_pages"], list)
+                self.assertTrue(validators.domain(domain), msg=f"Invalid domain: {domain}")
+            self.assertIn("web_pages", university, msg="University Web Pages are missing")
+            self.assertIsInstance(university["web_pages"], list, msg="University Web Pages must be a list")
             for web_page in university["web_pages"]:
-                self.assertTrue(validators.url(web_page))
-            self.assertIn("alpha_two_code", university)
-            self.assertIn("state-province", university)
-            self.assertIn("country", university)
+                self.assertTrue(validators.url(web_page), msg=f"Invalid web page: {web_page}")
+            self.assertIn("alpha_two_code", university, msg="University Alpha Two Code is missing")
+            self.assertIn("state-province", university, msg="University State/Province is missing")
+            self.assertIn("country", university, msg="University Country is missing")
 
+
+# Run tests locally
 if __name__ == "__main__":
     unittest.main()
